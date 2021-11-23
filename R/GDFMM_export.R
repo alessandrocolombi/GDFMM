6cf4f2de4d8957212fa8e925cf5b730a465f0f99
alessandrocolombi <58763854+alessandrocolombi@users.noreply.github.com>
2021-11-23 17:41:24 +0100

    Minor change in GDFMM_export.R

--- R/GDFMM_export.R
+++ R/GDFMM_export.R
@@ -1,13 +1,3 @@
-#' Title R function
-#'
-#' @param x input parameter
-#' @return returns x plus 10
-#' @export
-hello2 <- function(x) {
-  print("Hello, world!")
-  return(x+10)
-}
-
 #' The R function
 #'
 #' @param data input data.
